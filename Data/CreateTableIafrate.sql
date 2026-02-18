4462f31cf85781da9d6569eed8144c57539c0d81
Michaeliafrate <mli00003@mix.wvu.edu>
2026-02-18 18:28:10 -0500

    Created conference and team tables

---
+++ Data/CreateTableIafrate.sql
@@ -0,0 +1,15 @@
+CREATE TABLE ConferenceDivision (
+    CDID        INT PRIMARY KEY,
+    Conference  VARCHAR(20) NOT NULL,
+    Division    VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Team (
+    TID      INT PRIMARY KEY,
+    TName    VARCHAR(20) NOT NULL,
+    TCity    VARCHAR(20) NOT NULL,
+    TColor   VARCHAR(20),
+    TLogo    VARCHAR(20),
+    CDID     INT,
+    FOREIGN KEY (CDID) REFERENCES ConferenceDivision(CDID)
+);
